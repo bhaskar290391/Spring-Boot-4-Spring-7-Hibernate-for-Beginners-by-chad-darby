a6e9ca12779fba2ae625b24c8bd96a9156772fb9
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-04 20:24:46 +0530

    path variabke to fetch single students

--- Bhaskar_code/04-Spring-boot-crud-rest/01-spring-boot-crud-rest/src/main/java/com/practise/springboot/rest/StudentController.java
+++ Bhaskar_code/04-Spring-boot-crud-rest/01-spring-boot-crud-rest/src/main/java/com/practise/springboot/rest/StudentController.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practise.springboot.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
+
+	private List<Student> data;
+
+	@PostConstruct
+	public void constructData() {
+		data = new ArrayList<>();
+		data.add(new Student("Bhaskar", "Mudaliyar"));
+		data.add(new Student("Kanishk", "Soni"));
+		data.add(new Student("Soni ", "BHaskar"));
+	}
 
 	@GetMapping("/hello")
 	public String sayHello() {
@@ -20,12 +33,11 @@ public class StudentController {
 
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-
-		List<Student> data = new ArrayList<>();
-		data.add(new Student("Bhaskar", "Mudaliyar"));
-		data.add(new Student("Kanishk", "Soni"));
-		data.add(new Student("Soni ", "BHaskar"));
-
 		return data;
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudentData(@PathVariable int studentId) {
+		return data.get(studentId);
 	}
 }
