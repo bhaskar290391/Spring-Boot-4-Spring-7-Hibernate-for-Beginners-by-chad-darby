4c72ab7db51968f672ad9edeb4b7b1e1dba31dbd
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-04 14:26:45 +0530

    Running spring boot application using mvn and java -jar

---
+++ Bhaskar_code/01-Springboot-overview/05-command-line/src/main/java/com/practise/springboot/mycoolapp/rest/HelloController.java
@@ -0,0 +1,25 @@
+package com.practise.springboot.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run Hard 5k steps";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day";
+    }
+
+
+}
