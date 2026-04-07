57e13f1f261db076cecfcb35a8b78bbcf9d4d33e
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-07 13:11:10 +0530

    paging and sorting in spring data rest

--- Bhaskar_code/04-Spring-boot-crud-rest/04-spring-boot-crud-empployee-rest/src/main/java/com/practise/employee/dao/EmployeeRepository.java
+++ Bhaskar_code/04-Spring-boot-crud-rest/04-spring-boot-crud-empployee-rest/src/main/java/com/practise/employee/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.practise.employee.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.practise.employee.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
