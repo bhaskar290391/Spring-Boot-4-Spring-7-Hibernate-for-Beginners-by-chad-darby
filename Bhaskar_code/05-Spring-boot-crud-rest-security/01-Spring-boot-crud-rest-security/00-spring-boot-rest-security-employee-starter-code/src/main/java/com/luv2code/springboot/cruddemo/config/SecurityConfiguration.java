ad333e916b75b287880748f370d3d0f73af28cbb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-08 11:28:11 +0530

    adding security filter chain in the spring project with role based access

--- Bhaskar_code/05-Spring-boot-crud-rest-security/01-Spring-boot-crud-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/config/SecurityConfiguration.java
+++ Bhaskar_code/05-Spring-boot-crud-rest-security/01-Spring-boot-crud-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/config/SecurityConfiguration.java
@@ -1,16 +1,22 @@
 package com.luv2code.springboot.cruddemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
+	@Bean
 	public InMemoryUserDetailsManager manager() {
 
-		UserDetails bhaskar = User.builder().username("bhaskar").password("{noop}bhaskar").roles("ENPLOYEE").build();
+		UserDetails bhaskar = User.builder().username("bhaskar").password("{noop}bhaskar").roles("EMPLOYEE").build();
 
 		UserDetails kanishk = User.builder().username("kanishk").password("{noop}kanishk").roles("EMPLOYEE", "MANAGER")
 				.build();
@@ -19,5 +25,20 @@ public class SecurityConfiguration {
 				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
 		return new InMemoryUserDetailsManager(bhaskar, kanishk, samyuktha);
+	}
+
+	@Bean
+	public SecurityFilterChain security(HttpSecurity http) {
+
+		http.authorizeHttpRequests(config -> 
+				config.requestMatchers(HttpMethod.GET, "/api/employees")
+				.hasRole("EMPLOYEE").requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+		
+		http.httpBasic(Customizer.withDefaults());
+		http.csrf(csrf -> csrf.disable());
+		return http.build();
 	}
 }
