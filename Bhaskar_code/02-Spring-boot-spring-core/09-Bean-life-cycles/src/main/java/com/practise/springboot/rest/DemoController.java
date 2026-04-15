90040c41b6a98d776f82d80676f72dd24fb82843
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-15 08:40:16 +0530

    Bean life cycles

---
+++ Bhaskar_code/02-Spring-boot-spring-core/09-Bean-life-cycles/src/main/java/com/practise/springboot/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.practise.springboot.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practise.springboot.service.Coach;
+
+@RestController
+public class DemoController {
+
+	private Coach theCoach;
+
+	@Autowired
+	public void setTheCoach(@Qualifier("baseBallCoach") Coach theCoach) {
+		System.out.println("Inside the constructor ==>" + getClass().getSimpleName());
+		this.theCoach = theCoach;
+
+	}
+
+	@GetMapping("/dailyWorkout")
+	public String getDailyWorkout() {
+		return theCoach.getDailyWorkout();
+	}
+
+}
