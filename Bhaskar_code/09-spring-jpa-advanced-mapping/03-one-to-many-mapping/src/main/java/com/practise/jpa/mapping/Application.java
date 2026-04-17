eb343bc6037907000795ddb7ace5b46723aef3eb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-04-17 08:28:59 +0530

    adding course table into one to many mapping

---
+++ Bhaskar_code/09-spring-jpa-advanced-mapping/03-one-to-many-mapping/src/main/java/com/practise/jpa/mapping/Application.java
@@ -0,0 +1,80 @@
+package com.practise.jpa.mapping;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.practise.jpa.mapping.dao.AppDao;
+import com.practise.jpa.mapping.entity.Instructor;
+import com.practise.jpa.mapping.entity.InstructorDetails;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dataRunner(AppDao appDao) {
+		return runner -> {
+			deleteInstructorDetailById(appDao);
+			//findInstructorDetails(appDao);
+
+			// deleteInstructorById(appDao);
+			// getInstructor(appDao);
+			// createInstructor(appDao);
+		};
+	}
+
+	private void deleteInstructorDetailById(AppDao appDao) {
+		int instructorDetailsId=2;
+		appDao.deleteInstructorDetailById(instructorDetailsId);
+	}
+
+	private void findInstructorDetails(AppDao appDao) {
+
+		int instructorDetailsId = 2;
+
+		InstructorDetails instructorDetails = appDao.findInstructorDetailById(instructorDetailsId);
+		System.out.println(instructorDetails);
+
+		System.out.println("Instructor Deatils " + instructorDetails.getInstructor());
+	}
+
+	private void deleteInstructorById(AppDao appDao) {
+		int instructorId = 1;
+
+		
+		appDao.deleteInstructorById(instructorId);
+
+		System.out.println("Delete done !!!");
+
+	}
+
+	private void getInstructor(AppDao appDao) {
+
+		int instructorId = 1;
+
+		Instructor data = appDao.getInstructor(instructorId);
+
+		System.out.println(data);
+		System.out.println("Instructor Details Data " + data.getInstructorDetails());
+
+	}
+
+	private void createInstructor(AppDao appDao) {
+
+		Instructor instructor = new Instructor("bhaskar", "mudaliyar", "mudaliyar@gmail.com");
+		InstructorDetails details = new InstructorDetails("bhaskar@youtube.com", "cricket");
+
+		instructor.setInstructorDetails(details);
+
+		System.out.println("Saving an instructor");
+		appDao.save(instructor);
+		System.out.println("Done !!!");
+
+	}
+
+}
